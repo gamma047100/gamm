266bfd8b183efca222f39b7d0c2d5d5acb3b2f6e
1liochka1 <ila00001120@gmail.com>
2023-06-04 00:55:08 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+
+
+info = {'avax':('https://snowtrace.io/tx/','https://rpc.ankr.com/avalanche'),
+        'polygon':('https://polygonscan.com/tx/','https://polygon-rpc.com'),
+        'bsc':('https://bscscan.com/tx/','https://bscrpc.com')}
