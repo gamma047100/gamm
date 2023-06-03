f1eb0833be05f5e9f2bdebcfbc04e474e1bb3ac1
1liochka1 <ila00001120@gmail.com>
2023-06-04 01:03:06 +0300

    f

--- main.py
+++ main.py
@@ -138,23 +138,6 @@ class Bridger:
 
 
 def main():
-
-    '''
-
-     delay - ваше время (от и до) перерыва между кошельками
-     mode  -  0 - ваши сети (из и в какую) ; 1 - поиск нфт по наличию в сети и отправка в выбранную вами сеть
-
-     avax   /   bsc   /   polygon
-
-     chain - из какой сети
-     to - в какую из сетей (выбирается рандомно) - random.choice(['avax','polygon']) либо в конккретную сеть - просто имя сети 'avax'
-
-    '''
-
-    delay = (10, 600)
-    mode = 1
-    chain = 'bsc'          #   avax   /   bsc   /   polygon
-    to = 'avax'     #random.choice(['ваша сеть','ваша сеть'])
     logger.info(f'{" "*32}автор - https://t.me/iliocka{" "*32}')
     with open("keys.txt", "r") as f:
         keys = [row.strip() for row in f]
